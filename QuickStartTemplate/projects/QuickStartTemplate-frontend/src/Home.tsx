f9eca1ab7ceac1340523f1ad0811688fd05f69dd
vmosharova <varya.mosharova@gmail.com>
2025-09-15 14:18:54 +0200

    Refactor: rm CRA (Create React App) artifacts

--- QuickStartTemplate/projects/QuickStartTemplate-frontend/src/Home.tsx
+++ QuickStartTemplate/projects/QuickStartTemplate-frontend/src/Home.tsx
@@ -12,12 +12,6 @@ import HeroSection from './components/HeroSection'
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
-  const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
-  const [openPaymentModal, setOpenPaymentModal] = useState<boolean>(false)
-  const [openMintModal, setOpenMintModal] = useState<boolean>(false)
-  const [openTokenModal, setOpenTokenModal] = useState<boolean>(false)
-  const [openAppCallsModal, setOpenAppCallsModal] = useState<boolean>(false)
-  const [openUploadModal, setOpenUploadModal] = useState<boolean>(false)
 
   const { activeAddress } = useWallet()
 
